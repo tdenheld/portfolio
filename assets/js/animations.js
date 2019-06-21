11f6606ee6efeecd4a60b4bc1f3736c6b22d45b2
Tibor den Held <tibordenheld@Tibors-MacBook-Pro.local>
2019-06-21 07:53:23 +0200

    add scroll animation

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -70,7 +70,7 @@ function splitWords(className) {
             opacity: 0,
             y: -15,
             ease: Back.easeOut,
-        }, 0.05, "+=0");
+        }, 0.05, '+=0');
     };
 };
 $(function () {
@@ -88,6 +88,58 @@ function splitChars(className) {
         tl.staggerFrom(chars, 2, {
             opacity: 0,
             ease: Back.easeOut,
-        }, 0.01, "+=0");
+        }, 0.01, '+=0');
     };
 };
+
+
+// scroll magic
+// ------------------------------------------------------------------
+function scrollMagic() {
+    const section = $('.js-scroll-magic');
+    const richTxt = $('.js-scroll-magic-rt p');
+
+    var controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            duration: 99999,
+        },
+    });
+
+    function scrollTrig(obj) {
+        var hook = 0.94;
+        var customHook = $(obj).attr('hook');
+        if (customHook != null) {
+            hook = customHook;
+        };
+        var scrll = new ScrollMagic.Scene({
+                triggerElement: obj,
+            })
+            .triggerHook(hook)
+            .offset(0)
+            .on('start', function () {
+                $('.js-tr', obj).toggleClass('is-active');
+                if ($(obj).hasClass('js-tr')) {
+                    $(obj).toggleClass('is-active');
+                };
+            })
+            //.addIndicators()
+            .addTo(controller);
+    };
+
+    if (richTxt[0]) {
+        richTxt.each(function (i) {
+            $(this).addClass('js-tr tr-si tr-900');
+            scrollTrig(this);
+        });
+    };
+
+    if (section[0]) {
+        section.each(function (i) {
+            scrollTrig(this);
+        });
+    };
+};
+
+$(function () {
+    scrollMagic();
+});
