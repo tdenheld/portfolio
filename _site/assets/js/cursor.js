48c138d2bc7917bf172ddcd8b590af5b262ba458
Tibor den Held <tibor.denheld@ns.nl>
2019-09-11 20:19:21 +0200

    cleanup / setup cursor

---
+++ _site/assets/js/cursor.js
@@ -0,0 +1,61 @@
+function cursor() {
+    function follow(obj, x, y, t) {
+        TweenMax.to(obj, t, {
+            x: x,
+            y: y,
+            ease: Power4.easeOut
+        });
+    }
+
+    function fade(obj, a, b, t) {
+        TweenMax.fromTo(obj, t, {
+            autoAlpha: a
+        }, {
+            autoAlpha: b,
+            ease: Power4.easeOut
+        });
+    }
+
+    function tracking(obj, t) {
+        if ($(obj)[0]) {
+            $(document).mouseenter((e) => {
+                fade(obj, 0, 1, 0.1);
+                follow(obj, e.clientX, e.clientY, 0);
+            });
+            $(document).mouseleave(() => {
+                fade(obj, 1, 0, 0.7);
+            })
+            $(document).mousemove((e) => {
+                requestAnimationFrame(() => {
+                    follow(obj, e.clientX, e.clientY, t);
+                });
+            });
+        }
+    }
+    tracking('#js-cursor-outer', 0.5);
+    tracking('#js-cursor-inner', 0);
+
+    // hover states
+    // ------------------------------------------------
+    function sizing(obj, size) {
+        TweenMax.to(obj, 0.5, {
+            width: size,
+            height: size,
+            top: -size / 2,
+            left: -size / 2,
+            ease: Power4.easeOut
+        });
+    }
+
+    function hover(obj, size) {
+        const initSize = $(obj).width();
+        $('a, button, .js-settings-close').mouseover(() => {
+            sizing(obj, size);
+        });
+        $('a, button, .js-settings-close').mouseout(() => {
+            sizing(obj, initSize);
+        });
+    }
+    hover('#js-cursor-outer', 20);
+}
+cursor();
