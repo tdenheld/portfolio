5395f79fc9d2affcecab06c23af5f40d1394b9bc
Tibor den Held <tibor.denheld@ns.nl>
2019-09-22 22:40:11 +0200

    removed scrollmagic > custom script

---
+++ _site/assets/js/scroll.js
@@ -0,0 +1,69 @@
+function scroll() {
+    const section = $('.js-scroll');
+    const richTxt = $('.js-scroll-rt p');
+
+    const exec = {
+        add(obj) {
+            $('.js-tr', obj).addClass('is-active');
+            if ($(obj).hasClass('js-tr')) {
+                $(obj).addClass('is-active');
+            }
+        },
+        remove(obj) {
+            $('.js-tr', obj).removeClass('is-active');
+            if ($(obj).hasClass('js-tr')) {
+                $(obj).removeClass('is-active');
+            }
+        }
+    }
+
+    function scrollingListener(obj) {
+        const customHook = $(obj).attr('data-hook');
+        let hook = 0.94;
+
+        if (customHook != null) {
+            hook = customHook;
+        }
+
+        $(window).scroll(() => {
+            requestAnimationFrame(() => {
+                scroll(obj, hook);
+            });
+        });
+
+        $(window).resize(() => {
+            requestAnimationFrame(() => {
+                scroll(obj, hook);
+            });
+        });
+        scroll(obj, hook);
+    }
+
+    function scroll(obj, hook) {
+        const trigger = Math.round($(window).scrollTop() + $(window).innerHeight() * hook);
+        const pos = $(obj).offset().top;
+
+        if (pos <= trigger) {
+            exec.add(obj);
+        } else {
+            exec.remove(obj);
+        }
+    }
+
+    if (richTxt[0]) {
+        richTxt.each(function () {
+            $(this).addClass('js-tr tr-fi-up tr-1500');
+            scrollingListener(this);
+        });
+    }
+
+    if (section[0]) {
+        section.each(function () {
+            scrollingListener(this);
+        });
+    }
+}
+
+$(function () {
+    scroll();
+});
