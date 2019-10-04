e33309e742ce0856fec1282e99fcb783e4f22115
Tibor den Held <tibor.denheld@ns.nl>
2019-10-04 13:53:00 +0200

    revert to scrollMagic | edit typography settings

--- _site/assets/js/scroll.js
+++ _site/assets/js/scroll.js
@@ -2,69 +2,42 @@ function scroll() {
     const section = $('.js-scroll');
     const richTxt = $('.js-scroll-rt p');
 
-    function add(obj) {
-        $('.js-tr', obj).addClass('is-active');
-        if ($(obj).hasClass('js-tr')) {
-            $(obj).addClass('is-active');
-        }
-    }
+    var controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            duration: 99999,
+        },
+    });
 
-    function scrollingListener(obj) {
-        $(window).scroll(() => {
-            requestAnimationFrame(() => {
-                scroll(obj);
-            });
-        });
-
-        $(window).resize(() => {
-            requestAnimationFrame(() => {
-                scroll(obj);
-            });
-        });
-        scroll(obj);
-    }
-
-    function scrollingListener(obj) {
-        const customHook = $(obj).attr('data-hook');
-        let hook = 0.84;
-
+    function scrollTrig(obj) {
+        var hook = 0.94;
+        var customHook = $(obj).attr('data-hook');
         if (customHook != null) {
             hook = customHook;
         }
-
-        $(window).scroll(() => {
-            requestAnimationFrame(() => {
-                scroll(obj, hook);
-            });
-        });
-
-        $(window).resize(() => {
-            requestAnimationFrame(() => {
-                scroll(obj, hook);
-            });
-        });
-        scroll(obj, hook);
-    }
-
-    function scroll(obj, hook) {
-        const trigger = Math.round($(window).scrollTop() + screen.height * hook);
-        const pos = $(obj).offset().top;
-
-        if (pos <= trigger) {
-            add(obj);
-        }
+        var scrll = new ScrollMagic.Scene({
+                triggerElement: obj,
+            })
+            .triggerHook(hook)
+            .offset(0)
+            .on('start', function () {
+                $('.js-tr', obj).toggleClass('is-active');
+                if ($(obj).hasClass('js-tr')) {
+                    $(obj).toggleClass('is-active');
+                }
+            })
+            .addTo(controller);
     }
 
     if (richTxt[0]) {
         richTxt.each(function () {
             $(this).addClass('js-tr tr-fi-up tr-1500');
-            scrollingListener(this);
+            scrollTrig(this);
         });
     }
 
     if (section[0]) {
         section.each(function () {
-            scrollingListener(this);
+            scrollTrig(this);
         });
     }
 }
