bb85d017a26a12381b70afeef61931c4c0038ac5
Tibor den Held <tibor.denheld@ns.nl>
2019-11-04 09:11:22 +0100

    !!! --- REMOVED jQuery --- !!!

--- _site/assets/js/settings.js
+++ _site/assets/js/settings.js
@@ -1,102 +1,93 @@
-function settings() {
-    const obj = $('.js-settings');
-    const modal = $('.js-settings-modal');
-    const closeModal = $('.js-settings-close, .js-settings-submit');
-    const closeBtn = $('.js-settings-close');
-    const stag = $('.js-settings-stag');
+'use strict';
+
+(() => {
+    const obj = '.js-settings';
+    const modal = '.js-settings-modal';
+    const closeModal = '.js-settings-close, .js-settings-submit';
+    const closeBtn = '.js-settings-close';
+    const stag = '.js-settings-stag';
     const toggle = '.js-settings-toggle';
-    const label = $('.js-settings-label');
-    const body = $('html, body');
+    const label = '.js-settings-label';
+    const body = 'html, body';
     let active;
 
-    if (obj[0]) {
-        // transition of modal
-        // ------------------------------
-        const tl = new TimelineMax({
-            paused: true
-        });
-        tl.to(modal, 0.5, {
-            ease: Power4.easeInOut,
-            scaleX: 1,
-            display: 'block',
-        }).staggerFromTo(stag, 0.5, {
-            lazy: true,
-            autoCSS: true,
+    if (!exists(obj)) return;
+
+    // transition of modal
+    // ------------------------------
+    const tl = new TimelineMax({
+        paused: true
+    });
+    tl.to(modal, 0.5, {
+        ease: Power4.easeInOut,
+        scaleX: 1,
+        display: 'block',
+    }).staggerFromTo(stag, 0.5, {
+        lazy: true,
+        autoCSS: true,
+        opacity: 0,
+        x: 40
+    }, {
+        ease: Power3.easeInOut,
+        opacity: 1,
+        x: 0
+    }, 0.05, '-=0.2').to(closeBtn, 0.4, {
+        ease: Power3.easeInOut,
+        scale: 1,
+    }, '-=0.45');
+
+    // open close modal
+    // ------------------------------
+    ß(obj).map((el) => el.onclick = () => tl.play().timeScale(1));
+    ß(closeModal).map((el) => el.onclick = () => tl.reverse().timeScale(1.75));
+
+    // effect if clicked on an option
+    // ------------------------------
+    const toggleFX = () => ß(label).map((el) => {
+        const tl = new TimelineMax,
+            mySplitText = new SplitText(el, {
+                type: 'words,chars'
+            }),
+            chars = mySplitText.chars;
+
+        tl.staggerFromTo(chars, 0.6, {
             opacity: 0,
-            x: 40
+            x: 4
         }, {
-            ease: Power3.easeInOut,
             opacity: 1,
-            x: 0
-        }, 0.05, '-=0.2').to(closeBtn, 0.4, {
-            ease: Power3.easeInOut,
-            scale: 1,
-        }, '-=0.45');
+            x: 0,
+            ease: Back.easeOut
+        }, 0.03);
+    });
 
-        // open close modal
-        // ------------------------------
-        obj.click(() => {
-            tl.play().timeScale(1);
+    // store settings to local storage and activate
+    // ------------------------------
+    ß(toggle).map((el) => {
+        const toggleBtn = ß('.js-settings-btn', el);
+        const prop = el.getAttribute('data-toggle');
+        ß(body).map((el) => el.classList.add(localStorage.getItem(prop).trim()));
+
+        toggleBtn.map((el) => {
+            const thisVal = el.textContent.toLowerCase().trim();
+            const localVal = localStorage.getItem(prop).trim();
+            el.classList.remove('is-active');
+            if (thisVal === localVal) el.classList.add('is-active');
         });
-        closeModal.click(function () {
-            tl.reverse().timeScale(1.75);
+
+        const check = () => toggleBtn.map((el) => {
+            if (el.classList.contains('is-active')) active = el.textContent.toLowerCase().trim();
         });
 
-        // store settings to local storage and activate
-        // ------------------------------
-        function toggleFX() {
-            label.each(function () {
-                const tl = new TimelineMax,
-                    mySplitText = new SplitText(this, {
-                        type: 'words,chars'
-                    }),
-                    chars = mySplitText.chars;
-
-                tl.staggerFromTo(chars, 0.6, {
-                    opacity: 0,
-                    x: 4
-                }, {
-                    opacity: 1,
-                    x: 0,
-                    ease: Back.easeOut
-                }, 0.03);
-            });
-        }
-
-        $(toggle).each(function () {
-            const toggleBtn = $('.js-settings-btn', this);
-            const prop = $(this).attr('data-toggle');
-            body.addClass(localStorage.getItem(prop));
-
-            toggleBtn.each(function () {
-                const thisVal = $(this).text().toLowerCase();
-                const localVal = localStorage.getItem(prop);
-
-                if (thisVal == localVal) {
-                    toggleBtn.removeClass('is-active');
-                    $(this).addClass('is-active');
-                }
-            });
-
-            function check() {
-                toggleBtn.each(function () {
-                    if ($(this).hasClass('is-active')) {
-                        active = $(this).text().toLowerCase();
-                    }
-                });
-            }
-
-            toggleBtn.click(function () {
-                check();
-                toggleFX();
-                toggleBtn.removeClass('is-active');
-                $(this).toggleClass('is-active');
-
-                const val = $(this).text().toLowerCase();
-                localStorage.setItem(prop, val);
-                body.removeClass(active).addClass(localStorage.getItem(prop));
+        toggleBtn.map((el) => el.onclick = (e) => {
+            check();
+            toggleFX();
+            toggleBtn.map((el) => el.classList.remove('is-active'));
+            e.target.classList.add('is-active');
+            localStorage.setItem(prop, e.target.textContent.toLowerCase().trim());
+            ß(body).map((el) => {
+                el.classList.remove(active);
+                el.classList.add(localStorage.getItem(prop).trim());
             });
         });
-    }
-}
-settings();
+    });
+})()
