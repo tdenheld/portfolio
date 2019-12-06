aa35f92bfee74c4460371683efbb98860f64d4a8
Tibor den Held <tibor.denheld@ns.nl>
2019-12-06 10:58:57 +0100

    fix scrollTO

--- assets/js/generic.js
+++ assets/js/generic.js
@@ -57,19 +57,19 @@ const scrollToObject = () => {
     const offset = document.querySelector('.header').offsetHeight + 24;
     if (!exists(obj)) return;
 
-    ß(obj).map((el) => el.addEventListener('click', () => {
+    ß(obj).map((el) => el.onclick = () => {
         const target = el.getAttribute('href');
         if (!exists(target)) return;
         gsap.to(window, {
             duration: 0.8,
-            ease: 'power3.inOut',
+            ease: Power3.easeInOut,
             scrollTo: {
                 y: target,
-                offsetY: offset,
+                offsetY: offset
             }
         });
         return false;
-    }));
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
